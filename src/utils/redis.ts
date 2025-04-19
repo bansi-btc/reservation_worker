ea87d66dbbb92cd8e06f046495b5c2397969ce4b
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2025-04-19 13:13:53 +0530

    redis cloud provider added

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,5 +1,10 @@
 import { createClient } from "redis";
 
-export const redis = createClient();
+export const redis = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+  },
+});
 
 redis.on("error", (err) => console.error("Redis error:", err));
